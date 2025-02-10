f00ae2c22fccb2a11a9bf8f390aaa907238d22a8
anudevpm007 <anudevpm007@gmail.com>
2025-02-10 11:51:35 +0530

    324234

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"S+5RVSLoyJT/R4cIX8EfpeTEZh0nnlr2FlUiAsQ+vEc=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
         args: 'start',
         env_local: {
           APP_ENV: 'local', 
-          PORT:"80"
+          PORT:"3000"
         },
         env_dev: {
           APP_ENV: 'dev',
