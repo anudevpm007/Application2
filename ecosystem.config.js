1d1e6d1c7a7a787f2a73fcb2194518dc998979f8
anudevpm007 <anudevpm007@gmail.com>
2025-02-10 10:13:31 +0530

    after services

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,12 +8,15 @@ module.exports = {
         args: 'start',
         env_local: {
           APP_ENV: 'local', 
+          PORT:"80"
         },
         env_dev: {
-          APP_ENV: 'dev' // APP_ENV=dev
+          APP_ENV: 'dev',
+          PORT:"80" // APP_ENV=dev
         },
         env_prod: {
-          APP_ENV: 'prod' // APP_ENV=prod
+          APP_ENV: 'prod',
+          PORT:"80" // APP_ENV=prod
         }
       }
     ]
